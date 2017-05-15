d5850348a7fcc3a37a34cb8293650eaa5de4da69
Anne Brown <abbrownie@yahoo.com>
2017-05-15 00:25:59 +0000

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 
+(function($) {
+
+	skel.breakpoints({
+		xxlarge: '(max-width: 1920px)',
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 1000px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+	});
+
 	$(function() {
 
 		var	$window = $(window),
