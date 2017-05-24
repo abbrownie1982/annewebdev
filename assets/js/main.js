abecc82e6afa2cc1fa486a098c08fa41f9e2ba1f
Anne Brown <abbrownie@yahoo.com>
2017-05-24 16:09:03 +0000

    nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,17 @@
 
 	$(function() {
 
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 0);
+			});
 
 
 		// Touch mode.
